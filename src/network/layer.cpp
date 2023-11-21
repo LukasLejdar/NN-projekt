8debc7a1e64d442cd1296dc27dea8838941b7344
Lukáš Lejdar <lejdosl@gmail.com>
2023-11-21 12:20:32 +0100

    added moving averages matrices

--- src/network/layer.cpp
+++ src/network/layer.cpp
@@ -4,24 +4,32 @@
 Dense::Dense(const Dense& other): in_shape(other.in_shape), out_shape(other.out_shape) {
   w = other.w;
   b = other.b;
+  emaW = other.emaW;
+  emaB = other.emaB;
+  maW = other.maW;
+  maB = other.maB;
 }
 
 Dense::Dense(size_t in_shape, size_t out_shape): 
   in_shape(in_shape), 
   out_shape(out_shape),
   w(Matrix(out_shape, in_shape)),
-  b(Matrix(out_shape, 1)) {}
+  b(Matrix(out_shape, 1)),
+  emaW(Matrix(out_shape, in_shape)),
+  emaB(Matrix(out_shape, 1)),
+  maW(Matrix(out_shape, in_shape)),
+  maB(Matrix(out_shape, 1)) {}
 
-void Dense::swap(Dense& other) {
-    std::swap(in_shape, other.in_shape);
-    std::swap(out_shape, other.out_shape);
-    w.swap(other.w);
-    b.swap(other.b);
-  }
-
-Dense& Dense::operator=(const Dense& other) {
-  Dense temp(other);
-  swap(temp);
-  return *this;
-}
+//void Dense::swap(Dense& other) {
+//    std::swap(in_shape, other.in_shape);
+//    std::swap(out_shape, other.out_shape);
+//    w.swap(other.w);
+//    b.swap(other.b);
+//  }
+//
+//Dense& Dense::operator=(const Dense& other) {
+//  Dense temp(other);
+//  swap(temp);
+//  return *this;
+//}
 
--- src/network/layer.hpp
+++ src/network/layer.hpp
@@ -8,12 +8,14 @@ struct Dense {
   size_t in_shape, out_shape;
   Matrix w;
   Matrix b;
+  Matrix emaW, emaB; // moving averages
+  Matrix maW, maB;
 
-  Dense(): in_shape(0), out_shape(0), w({}), b({}) {}
+  Dense(): in_shape(0), out_shape(0), w({}), b({}), emaW({}), emaB({}), maW({}), maB({}) {}
   Dense(const Dense& other);
   Dense(size_t in_shape, size_t out_shape);
-  void swap(Dense& other);
-  Dense& operator=(const Dense& other);
+  //void swap(Dense& other);
+  //Dense& operator=(const Dense& other);
 };
 
 #endif
