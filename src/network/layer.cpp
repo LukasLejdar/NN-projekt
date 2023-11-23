b944f6fca4b6f21c9d6690f4b491f171df89de3c
Lukáš Lejdar <lejdosl@gmail.com>
2023-11-23 18:17:13 +0100

    implementd Model struct and adjusted cache for conv layers

--- src/network/layer.cpp
+++ src/network/layer.cpp
@@ -3,6 +3,7 @@
 Dense::Dense(const Dense& other): 
   in_shape(other.in_shape), 
   out_shape(other.out_shape),
+  w_shape(other.w_shape),
   w(other.w),
   maW(other.maW),
   emaW(other.emaW),
@@ -11,18 +12,20 @@ Dense::Dense(const Dense& other):
   emaB(other.emaB)
 {}
 
-Dense::Dense(size_t in_shape, size_t out_shape): 
-  in_shape(in_shape), 
-  out_shape(out_shape),
-  w(Matrix(out_shape, in_shape)),
-  maW(Matrix(out_shape, in_shape)),
-  emaW(Matrix(out_shape, in_shape)),
-  b(Matrix(out_shape, 1)),
-  maB(Matrix(out_shape, 1)),
-  emaB(Matrix(out_shape, 1))
+Dense::Dense(size_t in, size_t out): 
+  in_shape(in, 1), 
+  out_shape(out, 1),
+  w_shape(out, in),
+  w(Matrix(w_shape)),
+  maW(Matrix(w_shape)),
+  emaW(Matrix(w_shape)),
+  b(Matrix(out_shape)),
+  maB(Matrix(out_shape)),
+  emaB(Matrix(out_shape))
 {}
 
 Convolutional::Convolutional(const Convolutional& other): 
+  ker_shape(other.ker_shape),
   in_shape(other.in_shape), 
   out_shape(other.out_shape),
   k(other.k),
@@ -34,6 +37,7 @@ Convolutional::Convolutional(const Convolutional& other):
 {}
 
 Convolutional::Convolutional(Shape<3> in_shape, Shape<4> ker_shape):
+  ker_shape(ker_shape),
   in_shape(in_shape),
   out_shape(ker_shape[0], in_shape[1], in_shape[2]),
   k(ker_shape),
@@ -43,3 +47,49 @@ Convolutional::Convolutional(Shape<3> in_shape, Shape<4> ker_shape):
   maB(out_shape),
   emaB(out_shape)
 {}
+
+Model::Model(size_t conv_count, size_t dense_count, Convolutional* conv_layers, Dense* dense_layers) :
+  conv_count(conv_count), 
+  dense_count(dense_count), 
+  conv_layers(conv_layers), 
+  dense_layers(dense_layers) {}
+
+
+void initialize_cache(Cache& cache, Model& model) {
+  cache.conv.count = model.conv_count;
+  cache.conv.a = new Tensor<3>[model.conv_count+1]+1; // leave space for output
+  cache.conv.dA = new Tensor<3>[model.conv_count];
+  cache.conv.b = new Tensor<3>[model.conv_count];
+  cache.conv.dB = new Tensor<3>[model.conv_count];
+  cache.conv.k = new Tensor<4>[model.conv_count];
+  cache.conv.dK = new Tensor<4>[model.conv_count];
+
+  cache.dense.count = model.dense_count;
+  cache.dense.a = new Matrix[model.dense_count+1]; // leave space for preds 
+  cache.dense.dA = new Matrix[model.dense_count];
+  cache.dense.b = new Matrix[model.dense_count]; 
+  cache.dense.dB = new Matrix[model.dense_count];
+  cache.dense.w = new Matrix[model.dense_count]; 
+  cache.dense.dW = new Matrix[model.dense_count];
+
+  cache.conv.a[-1] = Tensor<3>(model.conv_layers[0].in_shape); // input
+  cache.dense.a[model.dense_count] = Matrix(model.dense_layers[model.dense_count].out_shape); // predictions
+
+  for(size_t i = 0; i < model.conv_count; i++) {
+    cache.conv.a[i] = Tensor<3>(model.conv_layers[i].in_shape); 
+    cache.conv.dA[i] = Tensor<3>(model.conv_layers[i].in_shape);
+    cache.conv.b[i] = Tensor<3>(model.conv_layers[i].out_shape); 
+    cache.conv.dB[i] = Tensor<3>(model.conv_layers[i].out_shape);
+    cache.conv.k[i] = Tensor<4>(model.conv_layers[i].k_shape); 
+    cache.conv.dK[i] = Tensor<4>(model.conv_layers[i].k_shape);
+  }
+
+  for(size_t i = 0; i < model.dense_count; i++) {
+    cache.dense.a[i] = Matrix(model.dense_layers[i].in_shape); 
+    cache.dense.dA[i] = Matrix(model.dense_layers[i].in_shape);
+    cache.dense.b[i] = Matrix(model.dense_layers[i].out_shape); 
+    cache.dense.dB[i] = Matrix(model.dense_layers[i].out_shape);
+    cache.dense.w[i] = Matrix(model.dense_layers[i].w_shape); 
+    cache.dense.dW[i] = Matrix(model.dense_layers[i].w_shape);
+  }
+}
--- src/network/layer.hpp
+++ src/network/layer.hpp
@@ -5,23 +5,70 @@
 #include "math.hpp"
 
 struct Dense {
-  const size_t in_shape, out_shape;
+  const Shape<2> in_shape, out_shape, w_shape;
   const Matrix w, maW, emaW; //moving averages
   const Matrix b, maB, emaB; 
 
   Dense(const Dense& other);
-  Dense(size_t in_shape, size_t out_shape);
+  Dense(size_t in, size_t out);
 };
 
 struct Convolutional {
   public:
-    const Shape<3> in_shape;
-    const Shape<3> out_shape;    
+    const Shape<4> k_shape;    
+    const Shape<3> in_shape, out_shape;
     const Tensor<4> k, maK, emaK;
     const Tensor<3> b, maB, emaB;
 
     Convolutional(const Convolutional& other);
     Convolutional(Shape<3> in_shape, Shape<4> ker_shape);
 };
+
+struct Model {
+  const size_t conv_count;
+  const size_t dense_count;
+  const size_t layers_count = conv_count + dense_count;
+
+  const Convolutional* conv_layers;
+  const Dense* dense_layers;
+
+  Model(size_t conv_count, size_t dense_count, Convolutional* conv_layers, Dense* dense_layers);
+};
+
+struct ConvCache {
+  size_t count;
+
+  Tensor<3> *a, *dA; //activations a[-1] is a duplicate of trained sample 
+  Tensor<3> *b, *dB;
+  Tensor<4> *k, *dK;
+
+  ~ConvCache() {
+    delete [] (a-1); delete [] dA;
+    delete [] b; delete [] dB;
+    delete [] k; delete [] dK;
+  }
+};
+
+struct DenseCache {
+  size_t count;
+
+  Matrix *a, *dA; // activations a[count] are predictions 
+  Matrix *b, *dB;
+  Matrix *w, *dW;
+
+  ~DenseCache() {
+    delete [] a; delete [] dA;
+    delete [] b; delete [] dB;
+    delete [] w; delete [] dW;
+  }
+};
+
+struct Cache {
+  size_t y; // corect label
+  ConvCache conv;
+  DenseCache dense;
+};
+
+void initialize_cache(Cache &cache, Model &model);
 
 #endif
