2869a337d6eb8336c9f0262de0ecfe8683c6b1d4
Lukáš Lejdar <lejdosl@gmail.com>
2023-11-25 22:00:03 +0100

    specialize Vector Tensor: even more epic tensors

--- src/network/tensor.hpp
+++ src/network/tensor.hpp
@@ -27,13 +27,22 @@ inline size_t multiply(const size_t* values, size_t count) {
   return result;
 }
 
+template <size_t dim, typename = void>
+struct Shape;
+
+template<class T, size_t dim, typename = void>
+struct TensorT; 
+
 template<size_t dim>
-struct Shape {
+struct Shape<dim, std::enable_if_t<(dim > 1)>> {
   size_t dimensions[dim];
   size_t size;
 
-  Shape(): dimensions(), size(0) {} 
-  Shape(const Shape& other): dimensions{}, size(other.size) {
+  const size_t& ht = *(dimensions+dim-2);
+  const size_t& wt = *(dimensions+dim-1);
+
+  Shape(): dimensions{}, size(0) {} 
+  Shape(const Shape& other): dimensions(), size(other.size) {
     std::copy(other.dimensions, other.dimensions + dim, dimensions);
   }
 
@@ -47,6 +56,11 @@ struct Shape {
     std::swap_ranges(dimensions, dimensions+dim, other.dimensions);
   }
 
+  void reshape(const size_t dims[dim]) {
+    std::copy(dims, dims + dim, dimensions);
+    size = multiply(dims, dim);
+  }
+
   size_t operator[](size_t index) {
     assert(index < dim);
     return dimensions[index];
@@ -57,14 +71,13 @@ struct Shape {
     return dimensions[index];
   }
 
-  bool operator == (const Shape& other) const {
-    return std::memcmp(dimensions, other.dimensions, dim* sizeof(size_t));
+  bool operator==(const Shape& other) const {
+    return std::equal(dimensions, dimensions+dim, other.dimensions, other.dimensions+dim);
   }
 };
 
 template<class T, size_t dim>
-struct TensorT {
-
+struct TensorT<T, dim, std::enable_if_t<(dim > 1)>> {
   Shape<dim> shape;
 
   const size_t* dimensions = shape.dimensions;
@@ -74,33 +87,41 @@ struct TensorT {
 
   T* v;
   
-  ~TensorT() { if(!is_subtensor) delete [] v; }
-  TensorT(): shape(), v(nullptr) {}
-  TensorT(bool is_subtensor): shape(), v(nullptr), is_subtensor(is_subtensor) {};
-  TensorT(const TensorT& other): shape(other.shape), v(new T[size]) {
+  ~TensorT() { 
+    if(!is_subtensor) {
+      //std::cout << "deleting " << ht << " - " << wt << "\n";
+      delete [] v; 
+    }
+  }
+
+  TensorT(): shape(), v(nullptr), is_subtensor(false) {}
+  TensorT(bool is_subtensor): shape(), v(nullptr), is_subtensor(is_subtensor){};
+  TensorT(const TensorT& other): shape(other.shape), v(new T[size]), is_subtensor(false) {
     std::copy(other.v, other.v+size, v);
   }
 
   template<typename... Args, typename = std::enable_if_t<(
       sizeof...(Args) == dim && std::is_convertible_v<T, size_t>)>>
-    TensorT(Args... args) : shape(args...), v(nullptr) { 
+    TensorT(Args... args) : shape(args...), v(nullptr), is_subtensor(false) { 
       v = new T[size];
       std::fill(v, v+size, 0); 
     }
 
-  TensorT(Shape<dim> shape) : shape(shape), v(new T[size]) { 
+  TensorT(Shape<dim> shape) : shape(shape), v(new T[size]), is_subtensor(false) { 
     std::fill(v, v+size, 0); 
   }
 
   template<typename... Args, typename = std::enable_if_t<(
       sizeof...(Args) == dim && std::is_convertible_v<T, size_t>)>>
-    TensorT(float *other_v, Args... args) : shape(args...), v(nullptr) { 
+    TensorT(float *other_v, Args... args) : shape(args...), v(nullptr), is_subtensor(false) { 
       v = new T[size];
       std::copy(other_v, other_v+size, v); 
     }
 
-  void setV(const T other_v[]) {
-    std::copy(other_v, other_v+size, v);
+  template<size_t dim2> 
+  void copyTensorOfSameSize(const TensorT<T, dim2> other) {
+    assert(other.size == size);
+    std::copy(other.v, other.v+size, v);
   }
 
   void faltten() {
@@ -108,45 +129,136 @@ struct TensorT {
     shape.dimensions[0] = size;
   }
 
+  TensorT<T, dim-1> operator[](size_t index) const {
+    //std::cout << "[] operator on " << ht << " - " << wt << "\n";
+    TensorT<T, dim-1> new_tensor(true);
+    new_tensor.shape.reshape(dimensions+1);
+    new_tensor.v = &v[index*new_tensor.size];
+
+    return new_tensor;
+  }
+
+  TensorT<T, 1> vectorize() {
+    //std::cout << "vectorizing " << ht << " - " << wt << "\n";
+    TensorT<T, 1> new_tensor(true);
+    new_tensor.shape.reshape(&size);
+    new_tensor.v = v;
+
+    return new_tensor;
+  }
+
+  void vectorize(TensorT<T, 1>& V) {
+    V.shape.reshape(&size);
+    V.set_is_subtensor(true);
+    delete [] V.v;
+    V.v = v;
+  }
+
+  TensorT& operator*(T scaler) {
+    for(size_t i = 0; i < size; i++) v[i] *= scaler;
+    return *this;
+  }
+
   void swap(TensorT& other) {
     shape.swap(other.shape);
     std::swap(other.v, v);
+    std::swap(other.is_subtensor, is_subtensor);
   }
 
-  void zero() {
-    std::fill(v, v+size, 0);
+  TensorT& operator=(const TensorT& other) {
+    TensorT temp = TensorT(other);
+    swap(temp);
+    return *this;
   }
 
-  void randomize(float mean=0, float variance=0) const {
-    if(variance == 0) variance = 1.0f/ht;
-    std::random_device rd;
-    std::mt19937 generator(rd());
-    std::normal_distribution<float> distribution(mean, std::sqrt(variance));
-
-    for(size_t i = 0; i < size; i++) v[i] = distribution(generator);
+  TensorT& operator=(TensorT& other) {
+    other.is_subtensor;
+    swap(other);
+    return *this;
   }
 
+  void set_is_subtensor(bool value) {
+    is_subtensor = value;
+  }
 
-  template<typename S = const TensorT>
-  typename std::enable_if<(S::_dim == 1), T>::type
-  const operator[](int index) const {
+  private:
+    static const size_t _dim = dim;
+    bool is_subtensor;
+
+};
+
+template<size_t dim>
+struct Shape<dim, std::enable_if_t<(dim == 1)>> {
+  size_t size;
+
+  Shape(): size(0) {} 
+  Shape(const Shape& other): size(other.size) {}
+  Shape(size_t size): size(size) {}
+
+  void swap(Shape& other) {
+    std::swap(size, other.size);
+  }
+
+  void reshape(const size_t dims[dim]) {
+    size = dims[0];
+  }
+
+  size_t operator[](size_t index) {
+    assert(index == 0);
+    return size;
+  }
+
+  size_t operator[](size_t index) const {
+    assert(index == dim);
+    return size;
+  }
+
+  bool operator==(const Shape& other) const {
+    return size == other.size;
+  }
+};
+
+template<class T, size_t dim>
+struct TensorT<T, dim, std::enable_if_t<dim == 1>> {
+  Shape<dim> shape;
+  const size_t& size = shape.size;
+  T* v;
+  
+  ~TensorT() { 
+    if(!is_subtensor) {
+      //std::cout << "deleting " << ht << " - " << wt << "\n";
+      delete [] v; 
+    }
+  }
+
+  TensorT(): shape(), v(nullptr), is_subtensor(false) {}
+  TensorT(bool is_subtensor): shape(), v(nullptr), is_subtensor(is_subtensor) {};
+  TensorT(const TensorT& other): shape(other.shape), v(new T[size]), is_subtensor(false) {
+    std::copy(other.v, other.v+size, v);
+  }
+
+  TensorT(size_t size) : shape(size), v(nullptr), is_subtensor(false) { 
+    v = new T[size];
+    std::fill(v, v+size, 0); 
+  }
+
+  TensorT(Shape<dim> shape) : shape(shape), v(new T[size]), is_subtensor(false) { 
+    std::fill(v, v+size, 0); 
+  }
+
+  TensorT(float *other_v, size_t size) : shape(size), v(nullptr), is_subtensor(false) { 
+    v = new T[size];
+    std::copy(other_v, other_v+size, v); 
+  }
+
+  template<size_t dim2> 
+  void copyTensorOfSameSize(const TensorT<T, dim2> other) {
+    assert(other.size == size);
+    std::copy(other.v, other.v+size, v);
+  }
+
+  T& operator[](int index) const {
     return v[index];
-  }
-
-  template<typename S = const TensorT>
-  typename std::enable_if<(S::_dim == 2), T*>::type
-  const operator[](int index) const {
-    return &v[index*wt];
-  }
-
-  template<typename S = const TensorT>
-  typename std::enable_if<(S::_dim > 2), TensorT<T, dim-1>>::type
-  operator[](size_t index) {
-    TensorT<T, dim-1> new_tensor(true);
-    std::copy(shape.dimensions+1, shape.dimensions+dim, new_tensor.shape.dimensions);
-    new_tensor.v = &v[index*multiply(shape.dimensions+1, dim-1)];
-
-    return new_tensor;
   }
 
   TensorT& operator*(float scaler) {
@@ -154,22 +266,31 @@ struct TensorT {
     return *this;
   }
 
+  void swap(TensorT& other) {
+    shape.swap(other.shape);
+    std::swap(other.v, v);
+    std::swap(other.is_subtensor, is_subtensor);
+  }
+
   TensorT& operator=(const TensorT& other) {
-    //std::cout << "const assignment " << other.ht << " - " << other.wt << "\n";
-    TensorT temp(other);
+    TensorT temp = TensorT(other);
     swap(temp);
     return *this;
   }
 
   TensorT& operator=(TensorT& other) {
-    //std::cout << "const assignment " << other.ht << " - " << other.wt << "\n";
-    TensorT temp(other);
-    swap(temp);
+    other.is_subtensor;
+    swap(other);
     return *this;
   }
+
+  void set_is_subtensor(bool value) {
+    is_subtensor = value;
+  }
+
   private:
     static const size_t _dim = dim;
-    bool is_subtensor = false;
+    bool is_subtensor;
 };
 
 #pragma GCC diagnostic pop
@@ -178,5 +299,6 @@ template <size_t dim>
 using Tensor = TensorT<float, dim>;
 
 typedef TensorT<float, 2> Matrix;
+typedef TensorT<float, 1> Vector;
 
 #endif
