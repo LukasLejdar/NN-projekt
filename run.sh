65a6abed3f53fff13cd299ff654aa0402e6acc0d
Lukáš Lejdar <lejdosl@gmail.com>
2023-12-01 11:59:57 +0100

    added run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-## change this file to your needs
 
-echo "Adding some modules"
+start_time=$SECONDS
 
-# module add gcc-10.2
-
-
+echo -en "\n"
 echo "#################"
 echo "    COMPILING    "
 echo "#################"
+echo -en "\n"
 
-## dont forget to use comiler optimizations (e.g. -O3 or -Ofast)
-# g++ -Wall -std=c++17 -O3 src/main.cpp src/file2.cpp -o network
+make _net
 
-
+echo -en "\n"
 echo "#################"
 echo "     RUNNING     "
-echo "#################"
+echo "################"
+echo -en "\n"
 
-## use nice to decrease priority in order to comply with aisa rules
-## https://www.fi.muni.cz/tech/unix/computation.html.en
-## especially if you are using multiple cores
-# nice -n 19 ./network
+stdbuf -oL nice -n 19 ./build/net 2>&1 | tee out
+cat out > /home/x548309/out
+
+elapsed_time=$((SECONDS - start_time))
+echo "Elapsed time: $elapsed_time seconds"
